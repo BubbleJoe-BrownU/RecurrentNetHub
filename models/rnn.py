95508633af47f425e38226767b470ca93d1c00bf
BubbleJoe-BrownU <jiayuzheng99@gmail.com>
2023-09-04 18:29:47 -0700

    multi-layer rnn model

--- models/rnn.py
+++ models/rnn.py
@@ -4,22 +4,51 @@ import torch.nn.functional as F
 from typing import Union
 
 class RNNBase(nn.Module):
+    """
+    Expects the inputs to be of shape (N, L, H_in), where N is the batch size, L is the sequence length, and H_in is the input embedding size
+    Returns output and hidden_state. Output is of shape (N, L, H_out) containing the hidden states of all elements in the sequence.
+    hidden_state is of shape (N, H_out) containing the final hidden state for each element in the batch.
+    """
 
     def __init__(self, input_size, hidden_size, nonlinearity: Union[nn.ReLU, nn.Tanh]):
         self.fc_i = nn.Linear(in_features=input_size, 
                               out_features= hidden_size)
         self.fc_h = nn.Linear(in_features=hidden_size, 
                               out_features=hidden_size)
-        
+        self.fc_o = nn.Linear(in_features=hidden_size, 
+                              out_features=hidden_size)
+        self.hidden_size = hidden_size
         self.nonlinearity = nonlinearity
+
+        # initialize all weights
+        self.apply(self._init_weights)
         
-    def forward(self, inputs, state):
-        hidden_state = self.nonlinearity(self.fc_i(inputs) + self.fc_h(state))
+    def forward(self, inputs, state=None):
+        if state is None:
+            state = torch.zeros([1, self.hidden_size], device=inputs.device)
+        outputs = torch.empty([inputs.shape[0], inputs.shape[1], self.hidden_size])
+        length = inputs.shape[1]
+        for i in range(length):
+            state = self.nonlinearity(self.fc_i(inputs[:, i, :]) + self.fc_h(state))
+            outputs[:, i, :] = state
+        
+        return outputs, state
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.uniform_(module.weight, -1*self.hidden_size**-0.5, 1*self.hidden_size**-0.5)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+
+
         
         
 
 class RNN(nn.Module):
     """
+    Expects the inputs to be of shape (N, L, H_in), where N is the batch size, L is the sequence length, and H_in is the input embedding size
+    Returns output and hidden_state. Output is of shape (N, L, H_out) containing the output features from the last layer of the RNN.
+    hidden_state is of shape (N, H_out) containing the final hidden state for each element in the batch.
     Parameters:
         input_size: the number of features in the inputs
         hidden_size: the number of features in the hidden states
@@ -27,27 +56,17 @@ class RNN(nn.Module):
             num_layer RNNs together, with each RNN layer taking in outputs of previous RNN layers as the input, 
             except for the first RNN layer, which takes in the original inputs
     """
-    def __init__(self, input_size: int, hidden_size: int, num_layers: int = 1):
+    def __init__(self, input_size: int, hidden_size: int, nonlinearity: Union[nn.ReLU, nn.Tanh], num_layers: int = 1):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.fc_s = nn.Linear(in_features=2*input_size, 
-                              out_features=input_size)
-        self.relu = nn.ReLU()
-        self.fc_o = nn.Linear(in_features=input_size, out_features=input_size)
+        self.layers = nn.ModuleList(RNNBase(input_size, hidden_size, nonlinearity) for _ in range(num_layers))
 
-    def forward(self, inputs, prev_states):
-        embed = torch.cat([inputs, prev_states], dim=-1)
-        state = self.relu(self.fc_s(embed))
-        output = self.relu(self.fc_o(embed))
+    def forward(self, inputs, state=None):
+        for layer in self.layers:
+            output, state = layer(inputs, state)
+            inputs = output
 
         return output, state
-    
-    def generate(self, seq):
-        length = seq.shape[1]
-        states = torch.zeros([1, 1, self.input_size])
-        for i in range(length):
-            inputs = seq[:, i, :]
-            outputs, states = self(inputs, states)
-            seq = torch.cat([seq, outputs], dim=1)
+
 
